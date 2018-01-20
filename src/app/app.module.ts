133fc8307173f9acc6774f42963b0bb3614a2c0d
Jethva, Mahesh <mjethvar8@gmail.com>
2018-01-20 09:17:07 -0500

    Configured title and added front page component
    - added title to the app.component.html called "From My Kitchen"
    - added styles to the app and the title
    - added the component for the front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule
